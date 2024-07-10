33b6ec95d0c399399bdc2d03e0e32433e6dc7e2c
tracer <tracey.chris@gmail.com>
2024-07-10 15:38:18 +1000

    jirify-Json | display a preview of json data loaded from file

--- jirify-json/jirify-json.js
+++ jirify-json/jirify-json.js
@@ -27,11 +27,43 @@ function handleFilePickerChange(event) {
         loadedJsonFilename = file.name;
         showFilename(loadedJsonFilename);
 
-        // loadJsonFile(file, handleCsvLoaded);
+        loadJSONFile(file, handleJsonLoaded);
     } else {
         showFilename('No file selected');
         showDataViewer('');
     }
+}
+
+function handleJsonLoaded(loadedJsonText) {
+    console.log('handle loaded Json data');
+
+    const loadedJson = JSON.parse(loadedJsonText);
+    console.log(loadedJson);
+
+    //display csv data as table
+    // const dataPresentationHTML = renderCSVDataAsTable(csvData);
+
+    //process loaded data to structured data model
+    // convertedJsonData = csvToJson(csvData);
+    const dataPresentationHTML = renderDataAsRawJson(loadedJson);
+
+    showDataViewer(dataPresentationHTML);
+    // showDownloadButton();
+}
+
+
+
+/*----------------------------------------
+  FILE LOADING
+  --------------------------------------*/
+
+function loadJSONFile(file, dataHandler) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const contents = e.target.result;
+        dataHandler(contents);
+    };
+    reader.readAsText(file);
 }
 
 
@@ -41,8 +73,23 @@ function handleFilePickerChange(event) {
   PRESENTATION LOGIC
   --------------------------------------*/
 
+function renderDataAsRawJson(jsonObject) {
+    console.log('jsonData (unprocessed): ', jsonObject);
+    const jsonString = JSON.stringify(jsonObject, null, 2);
+    const dataContent = "<pre>" + jsonString + "</pre>";
+    const title = "<hr><h3>JSON Preview</h3><br>";
+
+    const htmlContent = '' + title + dataContent;
+    return htmlContent;
+}
+
 function hideLoadCSVButton() {
     document.getElementById(loadJsonButtonId).style.display = 'none';
+}
+
+function showDataViewer(htmlContent) {
+    const elementId = 'dataViewer';
+    document.getElementById(elementId).innerHTML = htmlContent;
 }
 
 function showFilename(filename) {
