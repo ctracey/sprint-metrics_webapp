ca4e0f2724ec695b808dc4780ea84e613da22efd
tracer <tracey.chris@gmail.com>
2024-07-11 00:08:48 +1000

    jirify-json | update workitem model definition with more realistic values

--- jirify-json/workItemModelDefinition.js
+++ jirify-json/workItemModelDefinition.js
@@ -1,5 +1,18 @@
 const defaultWorkitemModelDefinition = { attributes: [
-    'id',
-    'summary',
-    'labels'
+    'Components',
+    'Labels',
+    'Sprint',
+    'Summary',
+    'Issue key',
+    'Issue Type',
+    'Status',
+    'Created',
+    'Updated',
+    'Custom field (Story Points)',
+    'Parent',
+    'Status Category',
+    'Status Category Changed',
+    'Resolution',
+    'Resolved',
+    'Custom field ([CHART] Time in Status)',
 ]};
